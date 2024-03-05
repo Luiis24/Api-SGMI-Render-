33b02056031a06bbef532911cf831f9f8e05b6f6
Luis <nandoarmo01@gmail.com>
2024-03-05 00:32:41 -0500

    b
    b

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fetch = require ('fetch');
-const dotenv = require('dotenv')
+const fetch = require('fetch');
+const dotenv = require('dotenv');
 const pg = require('pg');
 
 dotenv.config();
 
-app.use(express.json())
+app.use(express.json());
 
 const pool = new pg.Pool({
-    connetionString: process.env.DATABASE_URL
-})
+    connectionString: process.env.DATABASE_URL
+});
 
-
-app.use(cors())
-
+app.use(cors());
 
 const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
 const dataRoutes = require('./routes/dataRoutes');
 app.use('/', dataRoutes);
 
-
 app.use(bodyParser.json());
-
 
 const puerto = 4002;
 
-app.listen(puerto, () =>{
+app.listen(puerto, () => {
     console.log(`Servidor escuchando en ${puerto}`);
-})
+});
