b283291888eb410cee89029cf4cc2df9e3ad6471
Luis <134552997+Luiis24@users.noreply.github.com>
2024-03-10 20:58:58 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
  const CONFIG_BD ={
 user: 'postgres',
-host: 'viaduct.proxy.rlwy.net',
-database: 'railway',
-password: 'ebeabBdaBaE2bEdB41AGFg2e1B**5d45',
-port: 49025
+host: 'dpg-cnjabjicn0vc73c07jtg-a',
+database: 'gestion_de_mantenimiento',
+password: 'jDeSoRo9dbkrwTENMtDcoMXm6rGY6siF',
+port: 5432
 }
 
 module.exports = {
