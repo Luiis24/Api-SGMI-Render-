914b15d5032e271f3b7de08b8e8bc13daabad2a9
Luis <134552997+Luiis24@users.noreply.github.com>
2024-03-14 22:08:05 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
  const CONFIG_BD ={
-user: 'luis',
-host: 'dpg-cnjabjicn0vc73c07jtg-a',
-database: 'gestion_de_mantenimiento',
-password: 'jDeSoRo9dbkrwTENMtDcoMXm6rGY6siF',
+user: 'ls_morales',
+host: 'dpg-cnprkf6n7f5s73f6paug-a',
+database: 'gestion_de_mantenimiento_xohy',
+password: 'iAuQ3QaO8eWTn9FmqpuyVc4GzPjTv9cx',
 port: 5432
 }
 
