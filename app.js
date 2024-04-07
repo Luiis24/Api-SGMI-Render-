568e744b255df1c5921a8b0dac68e0ec6087274a
Luis <134552997+Luiis24@users.noreply.github.com>
2024-04-06 23:11:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fetch = require('fetch');
-const dotenv = require('dotenv');
-const pg = require('pg');
 
-dotenv.config();
-
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-});
-
 app.use(cors());
+
 
 const swaggerDocument = yaml.load(fs.readFileSync('./swagger.yaml', 'utf8'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+
 const dataRoutes = require('./routes/dataRoutes');
 app.use('/', dataRoutes);
 
+
 app.use(bodyParser.json());
+
 
 const puerto = 4002;
 
-app.listen(puerto, () => {
-    console.log(`Servidor escuchando en ${puerto}`);
-});
+app.listen(puerto, () =>{
+    console.log(`Servidor escuchando en http://localhost:${puerto}`);
+})
